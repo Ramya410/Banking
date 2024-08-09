d497b511f4c74c03d41892a23473bc5c979b9a3f
RamyaBhatL <RamyaBhatL@IBM-6DYNH24.in.ibm.com>
2024-08-09 17:24:04 +0530

    done

--- BankProject/src/main/java/com/example/bank/Controller/TransactionController.java
+++ BankProject/src/main/java/com/example/bank/Controller/TransactionController.java
@@ -1,5 +1,32 @@
 package com.example.bank.Controller;
 
-public class TransactionController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import com.example.bank.Model.Transaction;
+import com.example.bank.Service.TransactionService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
+        return ResponseEntity.ok(transactionService.createTransaction(transaction));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.getTransactionById(id));
+    }
+
+    @GetMapping
+    public List<Transaction> getAllTransactions() {
+        return transactionService.getAllTransactions();
+    }
 }
