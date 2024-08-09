6716d1d71f543bc34cc34b36b03004e20cdb3ef1
RamyaBhatL <RamyaBhatL@IBM-6DYNH24.in.ibm.com>
2024-08-09 17:10:12 +0530

    done

--- BankProject/src/main/java/com/example/bank/Model/Transaction.java
+++ BankProject/src/main/java/com/example/bank/Model/Transaction.java
@@ -1,5 +1,86 @@
 package com.example.bank.Model;
 
-public class Transaction {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+    private BigDecimal amount;
+    private LocalDateTime transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "source_account")
+    private Account sourceAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "destination_account")
+    private Account destinationAccount;
+
+    // Constructors, Getters, and Setters
+    public Transaction() {}
+
+    public Transaction(BigDecimal amount, Account sourceAccount, Account destinationAccount) {
+        this.amount = amount;
+        this.transactionDate = LocalDateTime.now();
+        this.sourceAccount = sourceAccount;
+        this.destinationAccount = destinationAccount;
+    }
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public Account getSourceAccount() {
+        return sourceAccount;
+    }
+
+    public void setSourceAccount(Account sourceAccount) {
+        this.sourceAccount = sourceAccount;
+    }
+
+    public Account getDestinationAccount() {
+        return destinationAccount;
+    }
+
+    public void setDestinationAccount(Account destinationAccount) {
+        this.destinationAccount = destinationAccount;
+    }
+
+	@Override
+	public String toString() {
+		return "Transaction [transactionId=" + transactionId + ", amount=" + amount + ", transactionDate="
+				+ transactionDate + ", sourceAccount=" + sourceAccount + ", destinationAccount=" + destinationAccount
+				+ "]";
+	}
+    
 }
