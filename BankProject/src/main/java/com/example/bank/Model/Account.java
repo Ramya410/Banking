14c533d530b01d771c9437f37657cd556acfdd25
RamyaBhatL <RamyaBhatL@IBM-6DYNH24.in.ibm.com>
2024-08-09 17:05:01 +0530

    done

--- BankProject/src/main/java/com/example/bank/Model/Account.java
+++ BankProject/src/main/java/com/example/bank/Model/Account.java
@@ -1,5 +1,65 @@
 package com.example.bank.Model;
 
-public class Account {
+import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountNumber;
+    private String accountType;
+    private BigDecimal balance;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    // Constructors, Getters, and Setters
+    public Account() {}
+
+    public Account(String accountType, BigDecimal balance, Customer customer) {
+        this.accountType = accountType;
+        this.balance = balance;
+        this.customer = customer;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
+
